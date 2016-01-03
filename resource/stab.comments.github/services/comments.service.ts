e1a6d71dcaf61470fc4947c7ab990248c7397cae
Sebastian Hönel <sebastian.honel@softwerk.se>
2016-01-03 19:10:38 +0100

    Add methods for posting and patching comments

--- resource/stab.comments.github/services/comments.service.ts
+++ resource/stab.comments.github/services/comments.service.ts
@@ -88,13 +88,120 @@ module Blog.Article.Comments {
 				if (optIssue.isEmpty || optComments.isEmpty) {
 					return new Common.Optional<Common.GithubComment[]>();
 				}
+				
+				const processedComments = optComments.get.map(comment => {
+					return StabGithubCommentsService.processComment(comment, optIssue.get);
+				});
 
-				const user = optIssue.get.user;
-				return new Common.Optional<Common.GithubComment[]>(optComments.get.map(comment => {
-					comment.isIssueOwner = comment.user.id === user.id;
-					return comment;
-				}));
+				// Set the comments to the issue:
+				optIssue.get.comments = processedComments;
+
+				return new Common.Optional<Common.GithubComment[]>(processedComments);
 			});
+		};
+
+		/**
+		 * Convenience wrapper. Patches (edits) a comment.
+		 * 
+		 * @see this.createOrPatchComment(..)
+		 */
+		public patchComment(issueUrl: string, body: string, commentId: number): angular.IPromise<Common.Optional<Common.GithubComment>> {
+			return this.createOrPatchComment(issueUrl, body, commentId);
+		};
+
+		/**
+		 * Convenience wrapper. Creates a comment.
+		 * 
+		 * @see this.createOrPatchComment(..)
+		 */
+		public createComment(issueUrl: string, body: string): angular.IPromise<Common.Optional<Common.GithubComment>> {
+			return this.createOrPatchComment(issueUrl, body).then(optComment => {
+				return this.issueByUrl(issueUrl).then(issue => {
+					// Prepend the new comment to its issue; comments are sorted from
+					// newest to oldest, so we have to unshift.
+					issue.get.comments.unshift(optComment.get);
+					return optComment;
+				});
+			});
+		};
+
+		/**
+		 * Creates or patches a new comment using authorization and returns it. The
+		 * action depends on whether the parameter 'commentId' is a number or not. If,
+		 * then the action is presumed to be 'patch'; 'create', otherwise.
+		 * 
+		 * @see https://developer.github.com/v3/issues/comments/#create-a-comment
+		 * @see https://developer.github.com/v3/issues/comments/#edit-a-comment
+		 */
+		private createOrPatchComment(issueUrl: string, body: string, commentId?: number): angular.IPromise<Common.Optional<Common.GithubComment>> {
+			issueUrl = StabGithubCommentsService.normalizeIssueUrl(issueUrl);
+			const issueApiUrl = StabGithubCommentsService.toApiIssueUrl(issueUrl);
+			
+			return this.authService.accessTokenUsingExistingAuthorizationOrAuthorize.then(optToken => {
+				if (!optToken.isPresent) {
+					return this.$q.when(new Common.Optional<Common.GithubComment>());
+				}
+
+				// POST or PATCH?
+				const isPatch = angular.isNumber(commentId);
+				const method = isPatch ? this.$http.patch : this.$http.post;
+
+				return method<Common.GithubComment>(issueApiUrl + '/comments' + (isPatch ? '/' + commentId : ''),
+				{ body: body }, {
+					headers: {
+						Authorization: 'token ' + optToken.get
+					}
+				}).then(promiseCallbackArg => {
+					return this.issueByUrl(issueUrl).then(optIssue => {
+						if (!optIssue.isPresent) {
+							throw new Error('The issue is not present.');
+						}
+						return new Common.Optional(
+							StabGithubCommentsService.processComment(promiseCallbackArg.data, optIssue.get));
+					});
+				}).catch(() => {
+					return this.$q.when(new Common.Optional<Common.GithubComment>());
+				});
+			});
+		};
+
+		/**
+		 * Transforms an issue-URL to a relative, normalized form. E.g.:
+		 * - https://github.com/MrShoenel/stab-gh-comments/issues/1
+		 *   becomes 'MrShoenel/stab-gh-comments/issues/1'
+		 * - /MrShoenel/stab-gh-comments/issues/1
+		 *   becomes 'MrShoenel/stab-gh-comments/issues/1'
+		 * 
+		 * We use this URL as the issue's ID.
+		 * 
+		 * @return string the normalized issue-URL.
+		 */
+		private static normalizeIssueUrl(issueUrl: string): string {
+			return (issueUrl + '').replace(/(?:https:\/\/github.com)?(?:\/)?([^/]+?\/[^/]+?\/issues\/[^/]+?)/i, "$1");
+		};
+
+		/**
+		 * Returns an absolute Github-API Issue-URL from a normalized URL.
+		 * 
+		 * @param normalizedIssueUrl string the normalized URL. The bahavior of
+		 *  this method is undefined for any non-normalized issue-URL.
+		 */
+		private static toApiIssueUrl(normalizedIssueUrl: string): string {
+			return 'https://api.github.com/repos/' + normalizedIssueUrl;
+		};
+
+		/**
+		 * Process a Github comment and does the following currently:
+		 * - Populate 'commenter'-property from 'user'-property
+		 * - Parse created- and updated-dates into JS-Dates
+		 * - Populate 'isIssueOwner'-property of comment
+		 */
+		private static processComment(comment: Common.GithubComment, issue: Common.GithubIssue): Common.GithubComment {
+			comment.commenter = comment.user; // populate standard-property
+			comment.created_at = new Date(Date.parse(comment.created_at.toString()));
+			comment.updated_at = new Date(Date.parse(comment.updated_at.toString()));
+			comment.isIssueOwner = comment.user.id === issue.user.id;
+			return comment;
 		};
 	};
 
